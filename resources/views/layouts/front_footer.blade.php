1c22bf052cac3c8248f1952036494b795d21fad0
Gerson Manuel Sugianto <manuelsugianto@gmail.com>
2024-12-11 11:53:43 +0700

    Update front_footer.blade.php

--- resources/views/layouts/front_footer.blade.php
+++ resources/views/layouts/front_footer.blade.php
@@ -6,7 +6,7 @@
           <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
                   <img src="{{ asset('assets/logo.png') }}" class="h-8 me-3" alt="FlowBite Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Senat Mahasiswa STIKOM Yos Sudarso</span>
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Senat Mahasiswa</span>
               </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
